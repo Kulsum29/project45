28a11dd73b202a06dc2b0107ad306e0207efb176
Kulsum29 <61190514+Kulsum29@users.noreply.github.com>
2023-09-13 18:01:30 +0530

    added animations for oggy and the fridge'

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # p5.play-boilerplate
 Boiler plate for p5.play
+
+# resources
+https://onlinepngtools.com/create-transparent-png
+https://ezgif.com/sprite-cutter
--- sketch.js
+++ sketch.js
@@ -1,12 +1,23 @@
+function preload(){
+  fridgeImg = loadImage("animations/fridge-img.png")
+  oggyAnimation = loadAnimation("animations/oggy1.png","animations/oggy2.png")
+  oggyWalkAnimation = loadAnimation("animations/oggy-w1.png","animations/oggy-w2.png","animations/oggy-w3.png","animations/oggy-w4.png")
+}
+
 function setup() {
   createCanvas(1000,700);
 
 
   fridge=createSprite(500,350,50,50);
-  fridge.shapeColor="mediumvioletred";
+  //fridge.shapeColor="mediumvioletred";
+  fridge.addImage("fr",fridgeImg)
+  fridge.scale=0.17
 
   oggy=createSprite(0,0,50,50);
-  oggy.shapeColor="deepskyblue";
+  //oggy.shapeColor="deepskyblue";
+  oggy.addAnimation("standing",oggyAnimation)
+  oggy.addAnimation("walking",oggyWalkAnimation)
+  oggy.scale=2
   edges=createEdgeSprites();
   
   joey=createSprite(70,502,50,50);
@@ -47,5 +58,17 @@ function draw() {
   }
   oggy.collide(edges);
 
+  if(oggy.velocityX!=0 || oggy.velocityY!=0){
+    oggy.changeAnimation("walking")
+    if(oggy.velocityX<0){
+      oggy.mirrorX(-1)
+    }
+    else{
+      oggy.mirrorX(1)
+    }
+  }
+  else{
+    oggy.changeAnimation("standing")
+  }
   drawSprites();
 }
